6d63aa1838baf28b8dd0ba5b0acafc06b5892115
Chris Hughes <redpandaxlbusiness@gmaill.com>
2024-05-05 21:53:05 -0700

    grid created

--- graphics.py
+++ graphics.py
@@ -22,9 +22,8 @@ class Window():
     def close(self):
         self.__running = False
     
-    def draw_line(self, line, fill_color):
+    def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
-
 
 
 class Point():
